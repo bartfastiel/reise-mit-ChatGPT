33a04b8cb0c4a33bbbd443da14360a000ecf88d4
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-20 10:03:44 +0200

    Setup project

---
+++ src/main/java/com/example/reisemitchatgpt/ReiseMitChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.reisemitchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReiseMitChatGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReiseMitChatGptApplication.class, args);
+    }
+
+}
